1560413a480b7a4315d5ad31edc59bc65867aa77
dubpub <dubpubz@gmail.com>
2015-11-11 22:34:09 +0300

    json support added

---
+++ src/Dubpub/Publisher/Helpers/functions.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: madman
+ * Date: 11.11.15
+ * Time: 22:04
+ */
--- tests/tests/php/.publisher.php
+++ tests/tests/php/.publisher.php
@@ -1,21 +1,17 @@
-<?php return array (
-  'demo-publisher/demo-package' => 
-  array (
-  ),
-  'dubpub/publisher' => 
-  array (
-    'assets' => 
-    array (
-      0 => 'assets/scripts       ->  {public,web,htdocs}/assets/js/',
-      1 => 'assets/styles        ->  {public,web,htdocs}/assets/css/',
-    ),
-    'configs' => 
-    array (
-      0 => 'configs/*.ini.dist -> configs/dist',
-    ),
-    'link' => 
-    array (
-      0 => '@link/* -> link',
-    ),
-  ),
-);
+<?php return [
+    "demo-publisher/demo-package" => [
+
+    ],
+    "dubpub/publisher" => [
+        "assets" => [
+            "assets/scripts       ->  {public,web,htdocs}/assets/js/",
+            "assets/styles        ->  {public,web,htdocs}/assets/css/"
+        ],
+        "configs" => [
+            "configs/*.ini.dist -> configs/dist"
+        ],
+        "link" => [
+            "@link/* -> link"
+        ]
+    ]
+];
