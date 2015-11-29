be8c15b2690b54d5c8c1b0ab21d0a5bcec60900d
dubpub <dubpubz@gmail.com>
2015-11-29 18:05:14 +0300

    gitgnore fix

---
+++ tests/tests/vendor/dubpub/publisher/.publisher.php
@@ -0,0 +1,14 @@
+<?php return [
+    'dubpub/publisher' => [
+        'assets' => [
+            'assets/scripts       ->  {public,web,htdocs}/assets/js/',
+            'assets/styles        ->  {public,web,htdocs}/assets/css/'
+        ],
+        'configs' => [
+            'configs/*.ini.dist -> configs/dist'
+        ],
+        'link' => [
+            '@link/* -> link'
+        ]
+    ]
+];
